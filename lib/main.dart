a5e8668c89f9af069cf8819b8402bafa247ee427
Zero Cold <ccarloos9@gmail.com>
2019-01-11 12:58:31 -0300

    inicio de projeto

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/telas/home_screen.dart';
+
+
+void main() => runApp(MaterialApp(
+      home: HomeScreen(),
+    ));
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('text'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
